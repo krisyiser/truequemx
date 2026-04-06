4a68df3f2c293722399ce8d33dc64116688c6ce9
krisyiser <krisyiser@users.noreply.github.com>
2026-04-05 22:08:52 -0600

    fix: Implemented src/middleware.ts and corrected Supabase SSR middleware helper

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,10 +1,10 @@
-﻿import { createServerClient } from "@supabase/ssr";
+import { createServerClient } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
-export const createClient = (request: NextRequest) => {
+export const updateSession = (request: NextRequest) => {
   let supabaseResponse = NextResponse.next({
     request: {
       headers: request.headers,
